a4a094c1402e45cdbf0c15ac32f6299069ca6b4b
aklimov <aklimov@cinimex.ru>
2019-10-03 01:13:24 +0300

    added wiremock

--- MOCKS/runwiremock.bat
+++ MOCKS/runwiremock.bat
@@ -1,2 +1,2 @@
-java -jar wiremock-standalone-2.12.0.jar --port 22223 --max-request-journal-entries 1000
+java -jar wiremock-standalone-2.24.0.jar --port 22223 --max-request-journal-entries 1000
 @pause
